11b65135f03d6f9c9622936b29ad1718182ec53f
fms <386273305@qq.com>
2016-09-22 18:03:10 +0800

    完成下拉列表

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+
+
+$(function(){
+	var $li=$('.xl').find('li').first();
+	var $xiala=$('.xiala');
+	var $xialali=$xiala.find('li');
+	var $xiala2=$('.xiala2')
+	$li.mouseover(function(){
+		$xiala.css('display','block')
+	});
+	$xiala.mouseover(function(){
+		$xiala.css('display','block')
+	});
+	
+	$xiala.mouseout(function(){
+		$xiala.css('display','none')
+		$xiala2.css('display','none')
+	});
+	
+	$li.mouseout(function(){
+		$xiala.css('display','none')
+	});
+	
+	$xialali.mouseover(function(){
+		$xiala2.css('display','block')
+	})
+	
+	$xiala2.mouseover(function(){
+		$xiala.css('display','block')
+		$xiala2.css('display','block')
+	})
+	
+	$xiala2.mouseout(function(){
+		$xiala2.css('display','none')
+		$xiala.css('display','none')
+	});
+	
+	//nav
+	$nav=$('.nav-xiala');
+	$navli=$('.nav-right').first('li');
+	$navli.mouseover(function(){
+		$nav.css('display','block');
+	});
+	$navli.mouseout(function(){
+		$nav.css('display','none')
+	})
+})
